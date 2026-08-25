bbd810119aefa1d04eb3920c0d879cd79a1747c6
Kiidoddity <markmercado25@hotmail.com>
2026-08-25 09:39:34 -0700

    Update cache name to daily-todo-auth-v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-todo-auth-v10';
+const CACHE_NAME = 'daily-todo-auth-v12';
 const STATIC_ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()));
