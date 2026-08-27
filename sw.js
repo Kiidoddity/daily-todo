789cd5639c76743536b728975416142d789708ad
Kiidoddity <markmercado25@hotmail.com>
2026-08-27 15:54:09 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'daily-todo-fastload-v17';
+const CACHE_NAME = 'daily-todo-historyfix-v18';
 const STATIC_ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(STATIC_ASSETS)).then(()=>self.skipWaiting()));
